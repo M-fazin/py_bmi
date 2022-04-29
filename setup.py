ae50038357c10690e4155dd8febed9b4396ba39f
Muhammed Fazin <86030159+M-fazin@users.noreply.github.com>
2022-04-29 20:33:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def readme(file="README.md"):
 
 setuptools.setup(
     name="Py_bmi",
-    version="1.0.8",
+    version="1.0.9",
     description="A Python module for Checking your bmi",
     long_description=readme(),
     long_description_content_type="text/markdown",
